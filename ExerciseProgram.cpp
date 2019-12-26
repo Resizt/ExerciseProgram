45abe92157f76cbf66d079a75da7ff639cfb811a
Resizt <resiztdzn@gmail.com>
2019-12-26 17:37:30 -0500

    The save function is updated
    For new members a seprate function is made for any new members created. This is setted for later functions that may include editing features.

--- ExerciseProgram.cpp
+++ ExerciseProgram.cpp
@@ -1,51 +1,60 @@
 #include <iostream>
 #include <sstream>
-#include <fstream>	
+#include <fstream>
+#include <stdlib.h>
 
 using namespace std;
 
-void newMember(string[10][5], int[10][4], int);
+void newMember	(string[10][1], string[10][5], float[10][4], int);
+void newSave	(string[10][1], string[10][5], float[10][4], int);
+void printUser	(string[10][1], string[10][5], float[10][4], int);
+void loadup		(string[10][1], string[10][5], float[10][4], int);
+
+void menu();
 
 int main(){
-	string	tINPUTS[10][5];
-	int		nINPUTS[10][4];
-	int		x = 1, choice = 0;
-	cout << "Exercise program"	<< endl;
-	cout << "-------------------------------------------" << endl;
+	string	uINPUTS[10][1], 
+			tINPUTS[10][5];
 
-	cout << "1. New User"		<< endl;
-	cout << "2. Existing User"	<< endl;
-	cout << "X. Exit Program"	<< endl;
+	float	nINPUTS[10][4];
+	int		x = 1, 
+			choice = 0;
+
+	loadup(uINPUTS, tINPUTS, nINPUTS, x);
 
 	while (choice != 3){
+		menu();
 		cin >> choice;
 		switch (choice){
 			case 1:
-				newMember(tINPUTS, nINPUTS, x);
-			//	newSave(tINPUTS, nINPUTS);
+				newMember(uINPUTS, tINPUTS, nINPUTS, x);
+				newSave(uINPUTS, tINPUTS, nINPUTS, x);
+				break;
 			case 2:
 			//	loadup();
 			//	calorielimit(input, x)
+				break;
 			case 3:
 				exit(0);
+				break;
 		}
 	}
+	cin >> choice;
 }
 
 
-void newMember(string tINPUTS[10][5], int nINPUTS[10][4], int x) { // Rename function to New User 
-	int bmi;
-	float bmi2;
+void newMember(string uINPUTS[10][1], string tINPUTS[10][5], float nINPUTS[10][4], int x) { // Rename function to New User 
+	float bmi;
 	string newUser;
 	ofstream newFile;
 
 	cout << "Enter your name please" << endl;
 	cin.ignore();
-	getline(cin, tINPUTS[x][1]);
+	getline(cin, uINPUTS[x][1]);
 
 
 	cout << "Choose your Sex" << endl;
-	getline(cin, tINPUTS[x][2]);
+	getline(cin, tINPUTS[x][1]);
 
 	cout << "Enter your Height in inches (So 5'5 would be 66)" << endl;
 	cin >> nINPUTS[x][1];
@@ -56,21 +65,21 @@ void newMember(string tINPUTS[10][5], int nINPUTS[10][4], int x) { // Rename fun
 	cout << "Enter your Weight (Just the numbers in lbs)" << endl;
 	cin >> nINPUTS[x][3];
 
-	bmi = nINPUTS[x][3] / (nINPUTS[x][1] * nINPUTS[x][1]);
-
+	bmi = (703 * (nINPUTS[x][3] / (nINPUTS[x][1] * nINPUTS[x][1])));
 	nINPUTS[x][4] = bmi;
 
-	//	bmi2 = (703 * nINPUTS[x][3]) / (nINPUTS[x][1] * nINPUTS[x][1])
-	//	nINPUTS[x][4] = bmi2;
+	printUser(uINPUTS, tINPUTS, nINPUTS, x);
+}
 
-	cout << "Your BMI is; " << bmi << endl;
-
-	newUser = tINPUTS[x][1];
+void newSave(string uINPUTS[10][1], string tINPUTS[10][5], float nINPUTS[10][4], int x) {
+	string newUser;
+	ofstream newFile;
+	newUser = uINPUTS[x][1];
 	newUser = newUser + ".txt";
 	newFile.open(newUser);
 
+	newFile << uINPUTS[x][1] << endl;
 	newFile << tINPUTS[x][1] << endl;
-	newFile << tINPUTS[x][2] << endl;
 	newFile << nINPUTS[x][1] << endl;
 	newFile << nINPUTS[x][2] << endl;
 	newFile << nINPUTS[x][3] << endl;
@@ -79,12 +88,29 @@ void newMember(string tINPUTS[10][5], int nINPUTS[10][4], int x) { // Rename fun
 
 	newFile.close();
 }
+
+void printUser(string uINPUTS[10][1], string tINPUTS[10][5], float nINPUTS[10][4], int x) {
+	system("CLS");
+	cout << "Welcome to the program, here's your record so far" << endl;
+	cout << "Name; "	<< uINPUTS[x][1] << endl;
+	cout << "Height; "	<< nINPUTS[x][1] << endl;
+	cout << "Weight; "	<< nINPUTS[x][3] << endl;
+	cout << "Age; "		<< nINPUTS[x][2] << endl;
+	cout << "BMI; "		<< nINPUTS[x][4] << endl;
+	cout << "------------------------------------------" << endl;
+}
+
+void menu() {
+	cout << "Exercise program" << endl;
+	cout << "-------------------------------------------" << endl;
+
+	cout << "1. New User" << endl;
+	cout << "2. Existing User" << endl;
+	cout << "X. Exit Program" << endl;
+}
+
 /*
 void calorielimit(string tINPUTS[10][6], int nINPUTS[10][5], int x) {
 	//if(input[10][6] < 80)
-}
-
-void BMIAdjustment(string tINPUTS[10][6], int nINPUTS[10][5], int x) {
-
 }
 */
